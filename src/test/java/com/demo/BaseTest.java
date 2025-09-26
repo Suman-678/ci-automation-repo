968f4d26feb03e1d3eb1a445fecf819972b36968
Suman-678 <suman.kumari17@wipro.com>
2025-09-27 00:51:22 +0530

    Update BaseTest.java

--- src/test/java/com/demo/BaseTest.java
+++ src/test/java/com/demo/BaseTest.java
@@ -20,7 +20,33 @@ public class BaseTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+                
+        // Common Chrome options
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--disable-features=VizDisplayCompositor");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-plugins");
+        options.addArguments("--disable-images");
+        options.addArguments("--disable-background-timer-throttling");
+        options.addArguments("--disable-backgrounding-occluded-windows");
+        options.addArguments("--disable-renderer-backgrounding");
+        options.addArguments("--window-size=1920,1080");
+        
+        // Unique user data directory for CI
+        String userDataDir = System.getProperty("java.io.tmpdir") + "/chrome-user-data-" + System.currentTimeMillis();
+        options.addArguments("--user-data-dir=" + userDataDir);
+        
+        // Environment-specific options
+        if (System.getProperty("ci.environment") != null) {
+            // CI environment - use headless mode
+            options.addArguments("--headless");
+        } else {
+            // Local environment - maximize window
+            options.addArguments("--start-maximized");
+        }
         tlDriver.set(new ChromeDriver(options));
         ScreenshotUtil.setDriver(getDriver());
     }
